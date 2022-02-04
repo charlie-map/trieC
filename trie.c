a0cd00a010c29559e2b5e41ac69cb652cf2f86d7
charlie-map <charjhall777@gmail.com>
2022-02-04 00:52:56 -0500

    finish insert -- needs testing

--- trie.c
+++ trie.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include "trie.h"
 
+// default next for simple_payload
+void *default_next(void *payload) {
+	if (!((char *) payload)[1])
+		return NULL;
+
+	return payload + sizeof(char);
+}
+
+// default converter for simple payload
 char simple_convert(void *payload) {
 	return ((char *) payload)[0];
 }
@@ -39,6 +48,12 @@ node_t *node_construct(void *payload) {
 	return new_node;
 }
 
+// dir = 1 to pull from curr_node->next
+// dir = 0 to pull from curr_node->prev
+node_t *pull_next(node_t *curr_node, int dir) {
+	return dir ? curr_node->next : curr_node->prev;
+}
+
 typedef struct Weight {
 	int weight_option; // chooses payload type (char or void *),
 				       // 1 for simple_weight (char), 0 for void *
@@ -50,6 +65,7 @@ typedef struct Weight {
 
 struct Trie {
 	weight_param weight_obj;
+	void (*next)(void *);
 
 	int (*weight)(struct Weight, void *, void *);
 
@@ -84,6 +100,9 @@ int major_weight(weight_param weight_obj, void *p1, void *p2) {
 		function will use char *'s as per default trie behavior
 		'p': payload type, defines what kind of data the trie holds: either
 		char (if given 'c') or void * (if given 'v')
+		'n': for an insertion (or search), this paramater takes a function
+		of type void (*)(void *) and gives the next value in the data type (
+		linked list, char *, etc.) -- will set to default (char *) if not inputted
 
 		param will look something like:
 			"-w -pc", int (*weight)(char, char)
@@ -101,6 +120,8 @@ trie_t *trie_create(char *param, ...) {
 
 	new_trie->children_option = 0;
 	new_trie->root_node = NULL;
+
+	new_trie->next = NULL;
 
 	va_list param_detail;
 	va_start(param_detail, param);
@@ -129,18 +150,81 @@ trie_t *trie_create(char *param, ...) {
 				weight_param.simple_weight = va_arg(param_detail, int (*)(char, char));
 			else
 				weight_param.heavy_weight = va_arg(param_detail, int (*)(void *, void *));
+		} else if (param[find_p + 1] == 'n') {
+			new_trie->next = va_arg(param_detail, void (*)(void *));
 		}
 	}
 
-	if (weight_param.weight_param && !weight_param.simple_weight) // default action
+	if (weight_param.weight_option && !weight_param.simple_weight) // default action
 		weight_param.simple_weight = default_weight;
+	if (!new_trie->next) // default
+		new_trie->next = default_next;
 
 	// return updated new_trie
 	return new_trie;
 }
 
-int trie_insertMETA(node_t *curr_node, weight_param weight_obj, node_t *insert_node, ...) {
+int match(int test1, int test2) {
+	return test1 && test2;
+}
+
+// 1 for insert right
+// 0 for insert left
+int inject_node(node_t *curr_node, node_t *new_node, int dir) {
+	node_t *curr_next_node = pull_next(curr_node, dir);
+	dir ? curr_node->next : curr_node->prev = new_node;
+	dir ? new_node->prev : curr_node->next = curr_node;
+
+	dir ? curr_next_node->prev : curr_next_node->next = new_node;
+	dir ? new_node->next : new_node->prev = curr_next_node;
+
+	return 0;
+}
+
+int trie_insertMETA(node_t *curr_node, trie_t *meta_func, void *value) {
 	// find position for the insert node:
+	node_t *start_node = curr_node;
+
+	int weight_ret = meta_func->weight(meta_func->weight_obj, value, curr_node->payload);
+	int search_direction = weight_ret > 0 ? 1 : -1, went_full_circ = 0;
+	while (weight_ret != 0) {
+		if (went_full_circ && start_node == curr_node)
+			break;
+
+		// check current position to see if we have found a position to insert
+		weight_ret = meta_func->weight(meta_func->weight_obj, value, curr_node->payload);
+
+		if (match(weight_ret > 0 && search_direction) || match(weight_ret < 0 && !search_direction)) // keep moving in a direction
+			curr_node = search_direction ? curr_node->next : curr_node->prev;
+		else
+			break;
+	}
+
+	if (weight_ret != 0) {
+		// insert node: right of curr_node if search_direction = 0,
+		// left of curr_node if search_direction = 1
+		node_t *new_node = node_construct(value);
+
+		inject_node(curr_node, new_node, search_direction);
+		curr_node = new_node;
+	}
+
+	void *get_next_value = meta_func->next(value);
+
+	if (!value) {
+		curr_node->end_weight += 1;
+		return 0;
+	}
+
+	curr_node->thru_weight++;
+
+	// check for existence of child
+	// if no child, need to build one bfore recurring
+	if (!curr_node->children)
+		curr_node->children = node_construct(get_next_value);
+
+	// recur
+	return trie_insertMETA(curr_node->children, meta_func, get_next_value);
 }
 
 // the value that comes after trie depends on weight_option
@@ -153,12 +237,10 @@ int trie_insert(trie_t *trie, ...) {
 
 	p_value = va_arg(get_value, void *);
 
-	node_t *insert_node = node_construct(p_value);
-
 	if (!trie->root_node)
 		trie->root_node = insert_node;
 	else
-		trie_insertMETA(trie->root_node, trie->weight_obj, insert_node);
+		trie_insertMETA(trie->root_node, trie->weight_obj, trie->next, insert_node);
 
 	return 0;
 }
